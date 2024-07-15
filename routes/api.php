023a6d8e52b4d1b399ac19d003e2d478b715592f
ClovisBN <clovisbonnemaison@gmail.com>
2024-07-15 17:43:43 +0200

    add JWT

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\IngredientController;
 
 
+// Accessible à tous
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 Route::apiResource('recipes', RecipeController::class);
 Route::apiResource('ingredients', IngredientController::class);
+//Seulement accessible via le JWT
+Route::middleware('auth:api')->group(function () {
+    Route::get('/currentuser', [UserController::class, 'currentUser']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
